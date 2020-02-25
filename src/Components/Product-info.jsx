97427488074921d367ba750b1defa8377c2eec3c
natalialvarezcorrea <natalia.37.na@gmail.com>
2020-02-25 08:31:27 -0500

    actualizado

--- src/Components/Product-info.jsx
+++ src/Components/Product-info.jsx
@@ -28,31 +28,30 @@ class ProductInfo extends React.Component{
         return(
             <div className=' derecha col-sm-12 col-lg-4'>
 
-                <div className="col-sm-10 col-lg-12 cuadro ">
+               
                   
                     <div className='col-12'>
-                    <div className='imagenes col-lg-6 col-sm-12 '> {Object.keys(items).length > 0 && 
+                    <div className='imagenes col-lg-6 col-sm-12 s '> {Object.keys(items).length > 0 && 
                             items.Resultados
                                 .imagenes_Producto
                                 .map(img => <img className='imagenes mt-3' src={img}/>)}
                     </div>
 
-                   <div className='tex col-lg-6 col-sm-12 mt-3'> 
+                   <div className='tex col-lg-6 col-sm-12 mt-3 a'> 
                        <p className='tit'>Titulo: {Object.keys(items).length > 0 && items.Resultados.titulo}</p>
-                       <p className='tit '>Precio: {Object.keys(items).length > 0 && items.Resultados.precio}</p>
+                       <p className='precio'>Precio: {Object.keys(items).length > 0 && items.Resultados.precio}</p>
                        
                    </div>
                    </div>
 
 
                    <div className='col-12 j '>
-                   <p className='tit' >Descripcion: <br/>{Object.keys(items).length > 0 && items.Resultados.descripcion}</p>
+                   <p className='tit' >Descripcion: <br/> <p >{Object.keys(items).length > 0 && items.Resultados.descripcion}</p></p>
                     </div>
                    
 
                     
 
-                </div>
 
                 
             </div>
