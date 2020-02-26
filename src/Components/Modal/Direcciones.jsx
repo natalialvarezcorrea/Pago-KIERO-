a22773c47ff533948c3da8fa046152e0d2b09dff
natalialvarezcorrea <natalia.37.na@gmail.com>
2020-02-26 09:27:41 -0500

    Add directions -in process yet

--- src/Components/Modal/Direcciones.jsx
+++ src/Components/Modal/Direcciones.jsx
@@ -10,37 +10,52 @@ class Direcciones extends React.Component{
 
   constructor(props){
     super(props);
-    this.state={
-      data:{},
-      nombre:'',
-      apellidos: '',
-      telefono:'',
-      ciudad:'',
-      barrio:'',
-      direccion:'',
+    this.state = {
+      data: {
+        user_id:"" ,
+        name_and_lastname: "",
+        department: "",
+        city: "",
+        neighborhood: "",
+        via: "",
+        number_via: "",
+        additional_data: "",
+        number_contact: "" 
+      },
       open:true
     }
+    this.destructured = [];
   }
 
   submitHandler = e => {
   e.preventDefault();
-  axios.post('https://kieroapi.net/user/getAddress/', this.state)
-  .then((response) => {
-    console.log(response);
-  }, (error) => {
-    console.log(error);
-  });
+  
+  axios.post(`https://kieroapi.net/createAddress/user/`, {
+
+    user_id : this.state.dataUser.user_id,
+    name_and_lastname : this.state.dataUser.name_and_lastname,
+    department : this.state.dataUser.department,
+    city : this.state.dataUser.city,
+    neighborhood : this.state.dataUser.neighborhood,
+    via : this.state.dataUser.via,
+    number_via : this.state.dataUser.number_via,
+    additional_data : this.state.dataUser.additional_data,
+    number_contact : this.state.dataUser.number_contact
+  }, 
+  {"Content-Type": "application/json"})
+  .then(res => { 
+      console.log(res.data);
+  })
+  .catch(error => console.log(error));
+  
 
 };
 
 
- 
 async componentWillMount(){
-
-    var destructured = [];
     var uri = window.location.href;
-    destructured = uri.substr(uri.indexOf("#")).split("/");
-    let user_id=destructured[4];
+    this.destructured = uri.substr(uri.indexOf("#")).split("/");
+    let user_id=this.destructured[4];
 
 
     let qData = {id: ""+user_id};
@@ -58,35 +73,37 @@ async componentWillMount(){
     // POR FAVOR QUITAR DATOS QUEMADOS Y OBTENERLOS DEL DOM
     // EEL ENDPOINT RETORNA: {"message":"ok"} EN CASO DE QUE TODO ESTE BIEN
     //////////////////////////////////////////////////////////////// 
-    if(destructured.length==6){
+    if(this.destructured.length==6){
         const data = {
-                    user_id: user_id,
+                    user_id:"2301220" ,
                     name_and_lastname: "Jhoubert Rincon",
                     department: "Bogotá D.C",
                     city: "Bogotá",
                     neighborhood: "Galerias",
                     via: "Cra12-34#56",
-                    number_via: "",
-                    additional_data: "",
+                    number_via: "calle 36 #81-22",
+                    additional_data: "casa primer pisd",
                     number_contact: "3118556404" 
                   }
-      axios.post(`https://kieroapi.net/createAddress/user/`, {data: data}, {"Content-Type": "application/json"})
-      .then(res => { 
-          console.log(res.data);
-      })
-      .catch(error => console.log(error));
+      
     }
     ////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////
-    
-
-
 }
 
+onSubmitNewDirection = () => {
+  let data = this.state.data;
+  axios.post(`https://kieroapi.net/createAddress/user/`, {data}, {"Content-Type": "application/json"})
+  .then(res => { 
+      console.log(res.data);
+  })
+  .catch(error => console.log(error));
+}
 
 changeHandler = e => {
-this.setState({ [e.target.name]: e.target.value });
+this.setState({...this.state.data,[e.target.name]: e.target.value });
+console.log(this.state)
 };
 
 
@@ -102,7 +119,7 @@ onCloseModal = () => {
 
 render(){
     const { open } = this.state;
-    const { nombre, telefono, ciudad, barrio, direccion } = this.state;
+    const { name_and_lastname, department, city, neighborhood, via,number_via,additional_data, number_contact } = this.state;
     //const { data } = this.state;
 
     return(
@@ -123,15 +140,18 @@ render(){
             </div>
                <a data-toggle="collapse" data-target="#demo" href="/" className="">Agrega una direccion</a>
                 <form id="demo" className="collapse">
-                    <input className="form-control " name='nombre'  type="text" value={nombre}placeholder="Nombre y apellido*" required onChange={this.changeHandler}/>
-                    <input className="form-control mt-3" name='telefono' value={telefono} type="text" placeholder="Telefono*" required onChange={this.changeHandler}/>
-                    <input className="form-control mt-3" name='ciudad' value={ciudad} type="text" placeholder="Ciudad*" required onChange={this.changeHandler}/>
-                    <input className="form-control mt-3" name='barrio' value={barrio} type="text" placeholder="Barrio*" required onChange={this.changeHandler}/>
-                    <input className="form-control mt-3" name='direccion' value={direccion} type="text" placeholder="Direccion*" required onChange={this.changeHandler}/>
+                    <input className="form-control " name='name_and_lastname'  type="text" value={name_and_lastname} placeholder="Nombre y apellido*" required onChange={this.changeHandler}/>
+                    <input className="form-control mt-3" name='department' value={department} type="text" placeholder="Departamento*" required onChange={this.changeHandler}/>
+                    <input className="form-control mt-3" name='city' value={city} type="text" placeholder="City*" required onChange={this.changeHandler}/>
+                    <input className="form-control mt-3" name='neighborhood' value={neighborhood} type="text" placeholder="Barrio*" required onChange={this.changeHandler}/>
+                    <input className="form-control mt-3" name='via' value={via} type="text" placeholder="Direccion*" required onChange={this.changeHandler}/>
+                    <input className="form-control mt-3" name='number_via' value={number_via} type="text" placeholder="Direccion* 2" required onChange={this.changeHandler}/>
+                    <input className="form-control mt-3" name='additional_data' value={additional_data} type="text" placeholder="Datos adicionales" onChange={this.changeHandler}/>
+                    <input className="form-control mt-3" name='number_contact' value={number_contact} type="text" placeholder="Numero de Contacto" required onChange={this.changeHandler}/>
                 </form>
 
             <div className={this.state.botone}>
-                <button type="submit" className="btn btn-outline-danger btn-block mt-3">Continuar</button>
+                <button type="submit" className="btn btn-outline-danger btn-block mt-3" onSubmit={this.onSubmitNewDirection} >Continuar</button>
             </div>
     </div>
    </Modal>
